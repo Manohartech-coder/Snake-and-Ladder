314ee9615103ecd70014d214d430aa21e0161cd7
Manohartech-coder <manoharreddyndl988@gmail.com>
2024-01-24 10:54:57 +0530

    added roll dice feature

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to play Snake and Ladder game");
-        playgame();
+        playGame();
     }
-    public static void playgame(){
+    private static  int rollDice(){
+        Random r=new Random();
+        return r.nextInt(6);
+    }
+    public static void playGame(){
+        Scanner sc=new Scanner(System.in);
+
         int position=0;
         System.out.println("Initial player position: "+position);
+        System.out.println("Enter to roll Die");
+        String s=sc.nextLine();
+        if(s.equalsIgnoreCase("roll")){
+           int diceVal=rollDice();
+           System.out.println("you rolled "+diceVal);
+        }
     }
 }
