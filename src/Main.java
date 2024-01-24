56c7eaf82ed5a273f14cd09d52baafcf272e6f65
Manohartech-coder <manoharreddyndl988@gmail.com>
2024-01-24 10:33:29 +0530

    initial position of player

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to play Snake and Ladder game");
+        playgame();
+    }
+    public static void playgame(){
+        int position=0;
+        System.out.println("Initial player position: "+position);
     }
 }
