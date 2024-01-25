232403e364e978c74b0ce41d640a439ff4dc3403
Manohartech-coder <manoharreddyndl988@gmail.com>
2024-01-25 10:27:15 +0530

    check for a play

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,49 @@
+import java.sql.SQLOutput;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to play Snake and Ladder game");
         playGame();
     }
+    //To roll a Die
     private static  int rollDice(){
         Random r=new Random();
         return r.nextInt(6);
     }
+    //Generate a Option
+    private static String checkFor(){
+        String[] arr={"No play","Ladder","Snake"};
+        Random r=new Random();
+        int i=r.nextInt(arr.length);
+        return arr[i];
+    }
+
+    //To play the game
     public static void playGame(){
         Scanner sc=new Scanner(System.in);
 
-        int position=0;
+        int position=0,diceVal=0;
         System.out.println("Initial player position: "+position);
         System.out.println("Enter to roll Die");
         String s=sc.nextLine();
+
         if(s.equalsIgnoreCase("roll")){
-           int diceVal=rollDice();
+           diceVal=rollDice();
            System.out.println("you rolled "+diceVal);
         }
+        String Option=checkFor();
+        switch (Option){
+            case "Ladder":
+                position+=diceVal;
+                break;
+            case "Snake":
+                position-=diceVal;
+                break;
+            case "No play":
+                break;
+            default:
+                System.out.println("u got nothing");
+        }
+        System.out.println("Now ur on the position:"+position);
     }
 }
